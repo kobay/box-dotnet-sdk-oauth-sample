3ff1859e5705e41e9db10d973c6e24382dd4f790
hiroyuki kobayashi <hkobayashi@box.com>
2021-03-29 22:33:20 +0900

    - add oath - add UI Elements

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using box_dotnet_sdk_oauth_sample.Models;
+using Box.V2;
+using Box.V2.Config;
 
 namespace box_dotnet_sdk_oauth_sample.Controllers
 {
     public class HomeController : Controller
     {
+        
+        // マイアプリ(標準OAuth）→ 構成 → クライアントIDとクライアントシークレット
+        private const string clientId = "asly78y1y07oux91k1ay2g3i8whpjq61";
+        private const string  clientSecret = "Jti29KH4L1tAJKa7rd0kX3PEI8DGHuHJ";
+        private const string  callBackUrl = "https://localhost:5001/Home/UIElements";
+        
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -20,6 +28,11 @@ namespace box_dotnet_sdk_oauth_sample.Controllers
 
         public IActionResult Index()
         {
+            
+            var config = new BoxConfig(clientId, clientSecret, new System.Uri(callBackUrl));
+            ViewBag.authorizationUrl = config.AuthCodeUri.ToString();
+                
+            // ViewBag.authorizationUrl = $"https://account.box.com/api/oauth2/authorize?client_id={clientId}&response_type=code";
             return View();
         }
 
@@ -28,6 +41,24 @@ namespace box_dotnet_sdk_oauth_sample.Controllers
             return View();
         }
 
+        public async Task<IActionResult> UIElements(String code)
+        {
+            Console.WriteLine("BoxRedirect {0}", code);
+            
+            var config = new BoxConfig(clientId, clientSecret, new System.Uri(callBackUrl));
+            var client = new BoxClient(config);
+            
+            await client.Auth.AuthenticateAsync(code);
+            var accessToken = client.Auth.Session.AccessToken;
+            //var refreshToken = client.Auth.Session.RefreshToken;
+
+            var user = await client.UsersManager.GetCurrentUserInformationAsync();
+            Console.WriteLine($"Login User: Id={user.Id}, Name={user.Name}, Login={user.Login}");
+
+            ViewBag.accessToken = accessToken;
+            return View();
+        }
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
