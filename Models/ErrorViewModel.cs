b0cfc109018c8def9ceb5e596bb50621b65e5bd9
hiroyuki kobayashi <hkobayashi@box.com>
2021-03-29 22:29:07 +0900

    dotnet new mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace box_dotnet_sdk_oauth_sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
